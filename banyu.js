3d093172269deb19607c832b1b183044e92e3631
wildan.mbp <wildan.ubg@gmail.com>
2016-08-24 06:01:56 +0800

    spelling banyu

---
+++ banyu.js
@@ -0,0 +1,49 @@
+var aql = require('alasql');
+var http = require("http");
+
+var url = "http://localhost/water_points.json";
+
+
+http.get("http://localhost/water_points.json", function(response){
+    //console.log(response.statusCode);
+
+    var body = '';
+
+    response.on('data', function(chunk) {
+        body += chunk;
+    });
+
+    response.on('end', function() {
+        var data = JSON.parse(body);
+        // console.log(data);
+
+        // var recap = aql("SELECT ROW _ FROM ?",[data]);
+        var func = aql("SELECT * FROM ? WHERE water_functioning == 'yes'", [data]);
+        var res = aql("SELECT * FROM ?",[data]);
+        var water_fn = aql("SELECT DISTINCT water_functioning as water_fn , COUNT(*) as number_fn FROM " +
+            "? GROUP BY water_functioning", [data]);
+        var comm = aql("SELECT DISTINCT communities_villages as village_name , COUNT(*) as number_water FROM " +
+            "? GROUP BY communities_villages", [data]);
+        var Bbroken = aql("SELECT communities_villages, COUNT(water_not_functioning) as nf FROM ? " +
+            "GROUP BY communities_villages ORDER BY nf ", [data]);
+
+        var max = aql("SELECT MAX(nf) FROM ?", [Bbroken]);
+        var maxx = max.valueOf();
+        var rank = aql("SELECT distinct(nf) as rank FROM ?", [Bbroken]);
+
+        var result = {
+            // dis : recap
+            num_water_points: res.length,
+            water_fn: water_fn,
+            number_functional: func.length,
+            community: comm,
+            water_points_broken: Bbroken,
+            rank: rank,
+
+        }
+
+        console.log(result)
+
+    });
+
+});
